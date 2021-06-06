00269cb82dce537679c82a57e242693694841881
Tony Kharioki <antolisah@gmail.com>
2021-06-06 03:09:12 +0300

    prisma-migrate

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      name: 'Tony',
+      email: 'tony@prisma.io',
+      posts: {
+        create: { title: 'Wassup Mr Stark' },
+      },
+      profile: {
+        create: { bio: 'I like movies' },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
